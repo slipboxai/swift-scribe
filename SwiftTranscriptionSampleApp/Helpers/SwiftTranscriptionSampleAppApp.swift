127c89e1a7575047c432a7d2f4cb21fd82228282
Brandon Weng <wengbrandon@gmail.com>
2025-06-11 13:56:03 -0400

    Initial commit

---
+++ SwiftTranscriptionSampleApp/Helpers/SwiftTranscriptionSampleAppApp.swift
@@ -0,0 +1,11 @@
+import SwiftData
+import SwiftUI
+
+@main
+struct SwiftTranscriptionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
